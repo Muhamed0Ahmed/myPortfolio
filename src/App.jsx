c72cfac18fc51dff69eeaa0d87fd494e0a47c59a
Muhamed0Ahmed <muhameed.ahmedd@gmail.com>
2024-04-29 18:18:46 +0300

    edit homepage in package.json

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="header">
+        <ul className='row'>
+          <li class="col-12 col-sm-6 col-md-4 col text-center ">Summery</li>
+          <li class="col-12 col-sm-6 col-md-4 col text-center">Home</li>
+          <li class="col-12 col-sm-6 col-md-4 col text-center">Skills</li>
+          <li class="col-12 col-sm-6 col-md-4 col text-center">Work Experience</li>
+          <li class="col-12 col-sm-6 col-md-4 col text-center">projects</li>
+          <li class="col-12 col-sm-6 col-md-4 col text-center">educations</li>
+          <li class="col-12 col-sm-6 col-md-4 col text-center">Contact Me</li>
+          <li class="col-12 col-sm-6 col-md-4 col text-center">item</li>
+        </ul>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
